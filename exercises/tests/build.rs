7f99a5549a487e488ea5408f47c108e19c64d69e
grl <857308047@qq.com>
2025-09-21 13:30:51 +0800

    test 7 8 9 暂时没认真看

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -9,16 +9,15 @@ fn main() {
     let timestamp = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
-        .as_secs(); // What's the use of this timestamp here?
-    let your_command = format!(
-        "Your command here with {}, please checkout exercises/tests/build.rs",
-        timestamp
-    );
-    println!("cargo:{}", your_command);
+        .as_secs(); // 这个时间戳用于设置 TEST_FOO 环境变量
+    
+    // 使用 rustc-env 命令设置 TEST_FOO 环境变量
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "Your command here, please checkout exercises/tests/build.rs";
-    println!("cargo:{}", your_command);
+    
+    // 使用 rustc-cfg 命令启用 "pass" 特性
+    println!("cargo:rustc-cfg=feature=\"pass\"");
 }
